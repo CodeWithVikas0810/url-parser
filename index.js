1db35de967a17ff6851a7b5213ad2255df70876f
Vikas Mehta <vikas.mehta.32.4@mountblue.tech>
2025-12-23 11:56:06 +0530

    short url

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -9,16 +10,47 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(express.urlencoded({
+  extended: false
+}));
+app.use(express.json());
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/shorturl', function (req, res) {
+  const originalUrl = req.body.url
+  let count = 0;
+  console.log(originalUrl)
+  let hostName = new URL(originalUrl).hostname;
+  dns.lookup(hostName, (err, address) => {
+    if (err) {
+      res.json({
+        error: 'invalid url'
+      })
+    } else {
+      res.json({
+        "original_url": originalUrl,
+        "short-url": ++count
+      })
+    }
+  })
+
+  // logger();
+})
+
+// const logger = function(req,res,next){
+//   console.log(req.body.url)
+//   next();
+// }
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/hello', function (req, res) {
+  res.json({
+    greeting: 'hello API'
+  });
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
